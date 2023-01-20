0cf73151d88f7c5a9fb33772dfdb5efc911116c4
Paul Bonhomme <80523797+pabonhomme@users.noreply.github.com>
2023-01-20 14:16:14 +0100

    Create Player.cs
    [DEV] start of the data access layer

---
+++ NavalWar1.API/NavalWar.DAL/Models/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NavalWar.DAL.Models
+{
+    public class Player
+    {
+
+        public Player()
+        {
+
+        }
+
+        public string Pseudo { get; set; }
+
+        public int Score { get; set; }
+    }
+}
