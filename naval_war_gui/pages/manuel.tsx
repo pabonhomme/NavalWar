474fcf099b3c6609f33d8db51bab44bc01942ee2
xuanzhe <3058634081@qq.com>
2023-01-27 20:41:20 +0100

    Compléter la page d'accueil et la structure de base

---
+++ naval_war_gui/pages/manuel.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+export default function manuel() {
+  return (
+    <>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          Back to home,bébé
+        </Link>
+      </h2>
+    </>
+  )
+}
