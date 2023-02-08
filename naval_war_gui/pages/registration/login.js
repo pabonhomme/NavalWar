e70841ad03c47b846f6b523b51543061b8f0c15a
Neil <62321203+CestMerNeil@users.noreply.github.com>
2023-02-09 00:03:53 +0100

    add fn to send POST of users

--- naval_war_gui/pages/registration/login.js
+++ naval_war_gui/pages/registration/login.js
@@ -2,92 +2,93 @@
  * @brief       To get the infos of the users
  * @author      Ao XIE
  * @date        2023.01.28
- * @version     1.0.0
+ * @version     1.1.0
  * @copyright   Copyright (c) 2023 XIE Ao. All rights reserved.
  *****************************************************************
  * @attention
  * Development environment: Windows
+ * Using Fetch API
  * @par Modification log:
  * <table>
  * <tr><th>Date        <th>Version  <th>Author    <th>Description
+ * <tr><td>2023/02/08  <td>1.1      <td>Ao XIE  <td>Get children infos, send POSTs.
  * <tr><td>2023/02/02  <td>1.0      <td>Ao XIE  <td>Creating the initial version
  * </table>
  ******************************************************************
  */
 
-import React from "react"
-import Image from 'next/image'
-import Link from "next/link"
-import { useState } from "react"
-import 'bootstrap/dist/css/bootstrap.css'
+import React from "react";
+import { useState } from "react";
+import 'bootstrap/dist/css/bootstrap.css';
+import styles from '../../styles/Home.module.css';
 
-import userDefaultPhoto from "./userDefaultPhoto.png";
-import ChooseBoat from "../chooseBoat/chooseBoat.js";
+import User from "./addUser.js";
 
 export default function Login(){
 
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [message, setMessage] = useState('');
+    // FUNCTIONS TO GET INFOMATIONS
+    const [firstNameUA, setFirsetNameUA] = useState('');
+    const [firstNameUB, setFirsetNameUB] = useState('');
+    const [lastNameUA, setLastNameUA] = useState('');
+    const [lastNameUB, setLastNameUB] = useState('');
 
-    const [user1, setUser1] = useState('');
-    const [user2, setUser2] = useState('');
+    const handleFA = (value) => {
+        setFirsetNameUA(value);
+    }
+    const handleFB = (value) => {
+        setFirsetNameUB(value);
+    }
+    const handleLA = (value) => {
+        setLastNameUA(value);
+    }
+    const handleLB = (value) => {
+        setLastNameUB(value);
+    }
 
-    const handleSubmit = (event) => {
+    //FUNCTIONS TO SEND POST
+    const handleSubmit = async(event) => {
         event.preventDefault();
-        
-        setMessage(`${user1} is been kept!`);
-        setFirstName('');
-        setLastName('');
+        const response = await fetch('siteweb', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                firstNameUA,
+                lastNameUA,
+                firstNameUB,
+                lastNameUB
+            })
+        });
+
+        if(!response.ok){
+            console.error('POST ERROR');
+            return;
+        }
+        const data = await response.json();
+        console.log('Data received: ', data);
     }
 
     return (
-        <form class="mb-3">
-            <Image 
-                src={userDefaultPhoto} 
-                class="rounded mx-auto d-block" 
-                alt="photo of users by default"
-            />
-            <label for="firstName" class="form-label">First Name</label>
-            <input 
-                type="text"
-                id="firstName"
-                class="form-control"
-                name="firstName"
-                value={firstName}
-                placeholder="First Name"
-                onChange={(event) =>
-                    setFirstName(event.target.value)
-                }
-            />
-            <label for="lastName" class="form-label">Last Name</label>
-            <input 
-                type="text"
-                id="lastName"
-                class="form-control"
-                name="lastName"
-                value={lastName}
-                placeholder="First Name"
-                onChange={(event) =>
-                    setLastName(event.target.value)
-                }
-            />
-
+        <form onSubmit={handleSubmit} className={styles.mainmanu}>
+            <div class="container">
+                <User 
+                    onFirstName={handleFA}
+                    onLastName={handleLA}
+                />
+                <br/>
+                <User
+                    onFirstName={handleFB}
+                    onLastName={handleLB}
+                />
+            </div>
+            <br/>
             <button 
-                type="submit" 
-                class="btn btn-primary"
-                onClick={handleSubmit}
-                >Submit</button>
-            <br />
-            <br />
-            <button type="submit" class="btn btn-primary">
-                <Link to="/ChooseBoat">Start</Link>
+                type="submit"
+                class="btn btn-light"
+            >
+                HERE WE GO!
             </button>
-
-            <br />
-            <br />
-
-            <h2>{message}</h2>
         </form>
     )
  }
